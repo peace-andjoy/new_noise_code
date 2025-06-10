6f594748f2b87240aea0853c894353bb3e50dc1a
peace-andjoy <86782505+peace-andjoy@users.noreply.github.com>
2025-06-10 15:41:57 +0800

    Add files via upload

---
+++ jipeng_code/robustness/test_fixseed.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J test_sigma      # job name, optional
+#SBATCH -N 1          # number of computing node
+#SBATCH -c 1          # number of cpus, for multi-thread programs
+#SBATCH --gres=gpu:1  # number of gpus allocated on each node
+#SBATCH -w node05  # apply for node05
+#SBATCH -o test_sigma.out
+
+# 定义要运行的脚本参数
+params=(
+    "--num_filters=16 --num_res=6 --sigma=0.05"
+)
+
+# 循环运行脚本
+for param in "${params[@]}"; do
+    python -u test_fixseed.py $param 2>&1
+    echo "Script completed: $param"
+done
